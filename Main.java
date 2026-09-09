a0c3689d9052b08d11d4753c44024dccef5b7475
maitemafekoeller-rgb <maitemafekoeller@gmail.com>
2026-09-09 19:50:13 +0200

    Korrektur des Wordings

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
    //Erstellung eines Objekts von der Klasse Girokonto
    Girokonto meinKonto = new Girokonto("DE19283020222", 14450.20, 100.0);
 
-   System.out.println("Kontoauszug");
+   System.out.println("Willkommen");
 
    //Methodenaufruf
    meinKonto.einzahlen(45);
